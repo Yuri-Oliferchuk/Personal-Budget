8ef6cb1e10e155c22d7dec3f2bd75452a7f0bff1
Yuriy Oliferchuk <94202324+Yuri-Oliferchuk@users.noreply.github.com>
2022-02-10 00:53:20 +0200

    Update server.js
    PORT and default page correction

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Rarse JSON request
 app.use(bodyParser.json());
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // Mount your existing apiRouter below at the '/api/' path.
 const apiRouter = require('./router/api.js');
 app.use('/api', apiRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+});
 
 // Start our server
 app.listen(PORT, () => console.log('Server started...'));
